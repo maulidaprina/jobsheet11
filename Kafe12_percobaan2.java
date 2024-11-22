93df495241e1b9a776bb5ab68b17e33ff4416956
Moona <ashadinoor@Moona.local>
2024-11-22 14:43:05 +0700

    Modifikasi kode

--- Kafe12_percobaan2.java
+++ Kafe12_percobaan2.java
@@ -1,20 +1,22 @@
 public class Kafe12_percobaan2 {
     
+    public static void main(String[] args) {
+        Menu("Muna", true, "DISKON50");
+    }
+    
     public static void Menu (String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
-        switch (kodePromo) {
-            case "DISKON50":
-                System.out.println("Kode promo berlaku: Anda mendapatkan diskon 50%!");
-                break;
-            case "DISKON30":
-                System.out.println("Kode promo berlaku: Anda mendapatkan diskon 30%!");
-                break;
-            default:
-                System.out.println("Kode promo tidak valid.");
+
+        if ("DISKON50".equals(kodePromo)) {
+            System.out.println("Kode promo berlaku: Anda mendapatkan diskon 50%.");
+        } else if ("DISKON30".equals(kodePromo)) {
+            System.out.println("Kode promo berlaku; Anda mendapatkan diskon 30%.");
+        } else {
+            System.out.println("Kode promo invalid.");
         }
 
         System.out.println("======== MENU RESTO KAFE========");
@@ -26,10 +28,6 @@ public class Kafe12_percobaan2 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("=================================");
         System.out.print("Silahkan pilih menu yang anda inginkan. ");
-    }
-    
-    public static void main(String[] args) {
-        Menu("Muna", true, "DISKON50");
     }
 }
     
